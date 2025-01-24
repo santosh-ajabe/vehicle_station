04bbe00efca95e63ed8cfd61fda37ff5e1e4aa0e
santosh-ajabe <120372941+santosh-ajabe@users.noreply.github.com>
2025-01-24 15:04:45 +0530

    vehicle modified

--- src/com/vehicle/ManageVehicle.java
+++ src/com/vehicle/ManageVehicle.java
@@ -1,11 +1,11 @@
 package com.vehicle;
 
-import java.io.*;
+
 import java.util.HashMap;
 
 public class ManageVehicle {
     private HashMap<String, Vehicle> vehicles;
-    private final String FILE_NAME = "vehicles.txt";
+    
 
     public ManageVehicle() {
         vehicles = new HashMap<>();
@@ -13,7 +13,7 @@ public class ManageVehicle {
 
     public void addVehicle(Vehicle vehicle) {
         vehicles.put(vehicle.getVehicleNumber(), vehicle);
-        saveVehiclesToFile();
+        
     }
 
     public void deleteVehicle(String vehicleNumber) {
@@ -37,22 +37,5 @@ public class ManageVehicle {
         return (vehicle != null) ? vehicle.toString() : "Vehicle not found.";
     }
 
-    public void saveVehiclesToFile() {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
-            oos.writeObject(vehicles);
-            System.out.println("Vehicle data saved successfully to " + FILE_NAME);
-        } catch (IOException e) {
-            System.err.println("Error saving vehicle data: " + e.getMessage());
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public void loadVehiclesFromFile() {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
-            vehicles = (HashMap<String, Vehicle>) ois.readObject();
-            System.out.println("Vehicle data loaded successfully from " + FILE_NAME);
-        } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Error loading vehicle data: " + e.getMessage());
-        }
-    }
 }
+
